3d706b0fc1d49ac61bb08a1e7cefd86a9af83a0f
cyber X santri <101439733+cyberchan27@users.noreply.github.com>
2022-03-30 22:06:24 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep
+echo 
+toilet -f big -F gay by Cyber X Santri Assyaiban
+echo $gred"selamat datang"
+echo
+sleep 2
+echo
+echo $yelow"silahkan di pilih"
+echo 
+echo $green"1.)kamu mau gk jadi pacar aku ?"
+echo
+echo $red"0.keluar aja"
+echo $white
+read -p"pilih yang mana : " cantik
+
+if [ $cantik = 1 ] || [$cantik = 1 ]
+then
+clear
+toilet -f big -F gay by Cyber X Santri Assyaiban
+echo $cyan"kalo mau bilang aja gk usah malu malutinggal bilang aja"
+sleep 2 
+alhamdulillah kalau mau
+fi
+
+if [$cantik = 0 ] || [$cantik = 0 ]
+then
+clear
+echo $green
+fi
+figlet "BYE"
+echo $cyan"goodbye hope you are happy with other than me"
+sleep 3
+exit
+fi
